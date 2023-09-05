adac16bf12c1eebf3092f79c843df911ff1426be
Enchan <enchant0130@gmail.com>
2023-09-05 22:47:31 +0900

    [Update] connect and read

--- main.go
+++ main.go
@@ -4,8 +4,37 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello,World!")
+
+	// 接続
+	connection, err := net.Dial("tcp", "koukoku.shadan.open.ad.jp:23")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 受信
+	buffer := make([]byte, 1024)
+	size, err := connection.Read(buffer)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for err == nil {
+		if size > 0 {
+			fmt.Print(string(buffer))
+		}
+
+		size, err = connection.Read(buffer)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	connection.Close()
 }
