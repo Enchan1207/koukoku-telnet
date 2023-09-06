3695c6841e7be4ab05580721f1da25d06ae875eb
Enchan <enchant0130@gmail.com>
2023-09-06 09:27:48 +0900

    [Fix] data decoding process

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Enchan1207/telnet-go
 
 go 1.20
+
+require golang.org/x/text v0.13.0 // indirect
--- main.go
+++ main.go
@@ -5,31 +5,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"time"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 func main() {
 
 	// 接続
-	connection, err := net.Dial("tcp", "koukoku.shadan.open.ad.jp:23")
+	host := "koukoku.shadan.open.ad.jp:23"
+	timeoutSec := 5
+	connection, err := net.DialTimeout("tcp", host, time.Duration(timeoutSec)*time.Second)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 受信
-	buffer := make([]byte, 1024)
+	// 受信開始
+	buffer := make([]byte, 32)
 	size, err := connection.Read(buffer)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for err == nil {
+	for size != 0 && err == nil {
 		if size > 0 {
-			fmt.Print(string(buffer))
+			fmt.Print(convertSjisToUtf8(buffer[:size]))
 		}
-
 		size, err = connection.Read(buffer)
 	}
 	if err != nil {
@@ -37,4 +40,13 @@ func main() {
 	}
 
 	connection.Close()
+}
+
+func convertSjisToUtf8(data []byte) string {
+	transformer := transform.NewReader(bytes.NewReader(data), japanese.ShiftJIS.NewDecoder())
+	u8str, err := ioutil.ReadAll(transformer)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(u8str)
 }
